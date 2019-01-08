4b31ba6a28040d818ea2730bfdadb2495b6624a4
MohammadL <mmlaif@gmail.com>
2019-01-08 08:13:44 +0300

    feat: Add dao file

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/database/dao/NameDao.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/database/dao/NameDao.java
@@ -1,8 +1,41 @@
 package com.mzdhr.androidthreadbackgroundtasks.database.dao;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.mzdhr.androidthreadbackgroundtasks.database.entity.NameEntity;
+
+import java.util.List;
+
 /**
  * Created by MohammadL on 08/1/2019
  * Contact me at mmlaif@gmail.com
  */
-public class NameDao {
+@Dao
+public interface NameDao {
+    @Query("SELECT * FROM names ORDER BY name_id DESC")
+    LiveData<List<NameEntity>> getAllNames();
+
+    @Query("SELECT * FROM names WHERE name_id = :id")
+    LiveData<NameEntity> getNameById(int id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertName(NameEntity nameEntity);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insertAndReturnColumnId(NameEntity nameEntity);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateName(NameEntity nameEntity);
+
+    @Delete
+    void deleteName(NameEntity nameEntity);
+
+    @Query("DELETE FROM names")
+    void deleteAll();
 }
