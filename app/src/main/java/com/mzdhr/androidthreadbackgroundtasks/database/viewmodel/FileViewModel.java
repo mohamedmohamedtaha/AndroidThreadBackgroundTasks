3959f2f2a7c50ecf34cb158ff7199cd00d87bdae
MohammadL <mmlaif@gmail.com>
2019-01-08 10:00:39 +0300

    feat: Add nameViewModel class

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/database/viewmodel/FileViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.mzdhr.androidthreadbackgroundtasks.database.viewmodel;
-
-/**
- * Created by MohammadL on 08/1/2019
- * Contact me at mmlaif@gmail.com
- */
-public class FileViewModel {
-}
---
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/database/viewmodel/NameViewModel.java
@@ -0,0 +1,51 @@
+package com.mzdhr.androidthreadbackgroundtasks.database.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.mzdhr.androidthreadbackgroundtasks.database.entity.NameEntity;
+import com.mzdhr.androidthreadbackgroundtasks.database.repository.NameRepository;
+
+import java.util.List;
+
+/**
+ * Created by MohammadL on 08/1/2019
+ * Contact me at mmlaif@gmail.com
+ */
+public class NameViewModel extends AndroidViewModel {
+    private NameRepository mRepository;
+    private LiveData<List<NameEntity>> mNames;
+
+    public NameViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = NameRepository.getInstance(application);
+        mNames = mRepository.getNames();
+    }
+
+    public LiveData<List<NameEntity>> getNames() {
+        return mNames;
+    }
+
+    public LiveData<List<NameEntity>> getNames(int start, int end) {
+        return mRepository.getNames(start, end);
+    }
+
+    public void insertName(NameEntity nameEntity) {
+        mRepository.insert(nameEntity);
+    }
+
+    public Long insertNameRetreveColumnId(NameEntity nameEntity) {
+        return mRepository.insertRetreveColumnId(nameEntity);
+    }
+
+    public void deleteName(NameEntity nameEntity) {
+        mRepository.delete(nameEntity);
+    }
+
+    public void deleteNames() {
+        mRepository.deleteAll();
+    }
+
+}
