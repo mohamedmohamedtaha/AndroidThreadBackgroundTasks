9479b60976a2c6169df304bff47cd1b127929cac
MohammadL <mmlaif@gmail.com>
2019-02-10 16:51:04 +0300

    feat: Notification Class updates

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/task/NotificationUtils.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/task/NotificationUtils.java
@@ -1,5 +1,6 @@
 package com.mzdhr.androidthreadbackgroundtasks.task;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.mzdhr.androidthreadbackgroundtasks.MainActivity;
@@ -28,7 +30,7 @@ public class NotificationUtils {
     private static final int NOTIFICAITON_ID = 1000;
 
     // Channel
-    private static final String NOTIFICAIOTN_CHANNEL = "The_Awesome_Channel_For_Notifications";
+    private static final String NOTIFICATION_CHANNEL_ID = "The_Awesome_Channel_For_Notifications";
 
     // Button Action
     private static final int ACTION_ONE = 1001;
@@ -52,6 +54,50 @@ public class NotificationUtils {
      * @param context
      */
     public static void showNotification(Context context) {
+        // Create a Notification Manager
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        // Create Notification Channel on Android 8.0 and Above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Prepare name, description, and importance (PRIORITY) for our channel. More about: Set the importance level at: https://developer.android.com/training/notify-user/channels#importance
+            String name = "Awesome Notification Channel";
+            String description = "This channel shows only awesome notification";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            // Create the Channel (Registering the channel with the system).
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+            notificationManager.createNotificationChannel(channel);
+            // Set its description
+            channel.setDescription(description);
+        }
+
+
+        // Create Notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+
+                // Title Text
+                .setContentTitle(text)
+                // Body Text
+                .setContentText(text)
+                // If you want more text use Style For longer text | Want more? Check: Create an Expandable Notification at: https://developer.android.com/training/notify-user/expanded
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(text))
+                // Intent to open when the user tap on the notification (We used our helper method):
+                .setContentIntent(contentIntent(context))
+                // Close the notification when user tap on it
+                .setAutoCancel(true)
+                ;
+
+
+        // More Configuration to the notificationBuilder
+        // Notification Intrusive (PRIORITY) on Android 7.1 and Lower.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        }
+
+        // Building the Notification
+
 
     }
 
