1654c1ff18c8f8061462c2da6a70417dc7b83d1d
MohammadL <mmlaif@gmail.com>
2019-02-13 15:05:09 +0300

    feat: WorkManager example done

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/Main8Activity.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/Main8Activity.java
@@ -1,9 +1,12 @@
 package com.mzdhr.androidthreadbackgroundtasks;
 
+import android.arch.lifecycle.Observer;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.firebase.jobdispatcher.Constraint;
@@ -13,11 +16,24 @@ import com.firebase.jobdispatcher.Job;
 import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.Trigger;
 import com.mzdhr.androidthreadbackgroundtasks.task.MyJobDispatcher;
+import com.mzdhr.androidthreadbackgroundtasks.task.MyJobManager;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 public class Main8Activity extends AppCompatActivity {
 
+    private static final String TAG = "Main8Activity";
     private static boolean sIsJobDispatcherInitialized;
 
     @Override
@@ -38,14 +54,14 @@ public class Main8Activity extends AppCompatActivity {
     }
 
 
-    private void firebaseJobDispatcher(){
+    private void firebaseJobDispatcher() {
         if (sIsJobDispatcherInitialized) {
             return;
         }
 
         // Time windows, should be a fields.
         int INTERVAL_MINUTES = 1;
-        int INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(INTERVAL_MINUTES)) ;
+        int INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(INTERVAL_MINUTES));
         int SYNC_FLEXTIME_SECONDS = INTERVAL_SECONDS;
 
         GooglePlayDriver driver = new GooglePlayDriver(this);
@@ -73,10 +89,95 @@ public class Main8Activity extends AppCompatActivity {
         sIsJobDispatcherInitialized = true; // the job has been initialized.
     }
 
-    private void cancelFirebaseJobDispatcher(){
+    private void cancelFirebaseJobDispatcher() {
         GooglePlayDriver driver = new GooglePlayDriver(this);
         FirebaseJobDispatcher firebaseJobDispatcher = new FirebaseJobDispatcher(driver);
         firebaseJobDispatcher.cancel("MY_JOB_TAG");
     }
+
+
+    private void workManager() {
+
+
+        // Passing data to our works
+        Data myData = new Data.Builder()
+                .putString("NAME_KEY", "Mohammad")
+                .putInt("AGE_KEY", 14)
+                .build();
+
+
+        // This gonna run your work right away.
+        // OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(MyJobManager.class).build();
+
+        // Constraints for our work
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresDeviceIdle(true)
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+
+
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(MyJobManager.class)
+                .setInputData(myData)
+                .setConstraints(constraints)
+                .addTag("MY_WORK_MANAGER_TAG_ONE_TIME")
+                .build();
+
+        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyJobManager.class, 12, TimeUnit.HOURS)
+                .setInputData(myData)
+                .setConstraints(constraints)
+                .addTag("MY_WORK_MANAGER_PERIODIC_TAG")
+                .build();
+
+        // Send our work to be scheduled.
+        WorkManager.getInstance().enqueue(oneTimeWorkRequest);
+        WorkManager.getInstance().enqueue(periodicWorkRequest);
+
+
+        // Canceling our jobs by their Id
+        // Get Id for our jobs (after we enqueue them)
+        UUID oneTimeWorkRequestId = oneTimeWorkRequest.getId();
+        WorkManager.getInstance().cancelWorkById(oneTimeWorkRequestId);
+
+        UUID periodicWorkRequestId = periodicWorkRequest.getId();
+        WorkManager.getInstance().cancelWorkById(periodicWorkRequestId);
+
+
+
+
+    }
+
+    private void getResultWorkManager() {
+        WorkManager.getInstance().getWorkInfosByTagLiveData("MY_WORK_MANAGER_TAG_ONE_TIME")
+                .observe(this, new Observer<List<WorkInfo>>() {
+                    @Override
+                    public void onChanged(@Nullable List<WorkInfo> workInfos) {
+                        if (workInfos != null && workInfos.get(0).getState().isFinished()) {
+                            String result = workInfos.get(0).getOutputData().getString("RESULT");
+                            Log.d(TAG, "onChanged: " + result);
+                        }
+                    }
+                });
+    }
+
+    private void cancelWorkMananger() {
+        WorkManager.getInstance().cancelAllWork();
+        WorkManager.getInstance().cancelAllWorkByTag("MY_WORK_MANAGER_TAG_ONE_TIME");
+    }
+
+    private void chaningWorkManager() {
+        // If any task returns Result.failure(), the whole sequence ends.
+        WorkManager.getInstance()
+                .beginWith(downLoadImageWorkRequest)
+                .then(saveDownloadedImageToDatabaseWorkRequest)
+                .then(cleanTempWorkRequest)
+                .enqueue();
+
+        WorkManager.getInstance()
+                .beginWith(Arrays.asList(downloadImagesWorkRequest, downloadTextsWorkRequest, downloadLinksWorkRequest))    // Runs in Parallel.
+                .then(cleanTempWorkRequest)
+                .enqueue();
+    }
+
+
 
 }
--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/task/MyJobManager.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/task/MyJobManager.java
@@ -1,8 +1,40 @@
 package com.mzdhr.androidthreadbackgroundtasks.task;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
 /**
  * Created by MohammadL on 13/2/2019
  * Contact me at mmlaif@gmail.com
  */
-public class MyJobManager {
+public class MyJobManager extends Worker {
+
+    public MyJobManager(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        String name = getInputData().getString("NAME_KEY");
+        int age = getInputData().getInt("AGE_KEY", -1);
+
+        // Our Code goes here
+        String result = name + age;
+
+        Data output = new Data.Builder()
+                .putString("RESULT_KEY", result)
+                .build();
+
+        //return Result.failure();// Tells WorkManager to not do this task again.
+        //return Result.retry();  // Tells WorkManager to do this task again.
+        //return Result.success();
+
+        return Result.success(output);
+    }
+
 }
