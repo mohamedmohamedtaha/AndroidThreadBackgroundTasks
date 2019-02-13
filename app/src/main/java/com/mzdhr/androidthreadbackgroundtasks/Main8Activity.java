33d1133ff4bfc461973266dfa8ba0ab7104c9964
MohammadL <mmlaif@gmail.com>
2019-02-13 11:45:57 +0300

    refactor: move class

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/Main8Activity.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/Main8Activity.java
@@ -1,4 +1,4 @@
-package com.mzdhr.androidthreadbackgroundtasks.thread;
+package com.mzdhr.androidthreadbackgroundtasks;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,7 +12,6 @@ import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.Trigger;
-import com.mzdhr.androidthreadbackgroundtasks.R;
 import com.mzdhr.androidthreadbackgroundtasks.task.MyJobDispatcher;
 
 import java.util.concurrent.TimeUnit;
