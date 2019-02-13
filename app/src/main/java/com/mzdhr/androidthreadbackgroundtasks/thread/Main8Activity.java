03f7ab7e2e581b637956b9cdd804125299619283
MohammadL <mmlaif@gmail.com>
2019-02-13 11:05:54 +0300

    feat: Firebase JobDispatcher example done

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/thread/Main8Activity.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/thread/Main8Activity.java
@@ -2,14 +2,24 @@ package com.mzdhr.androidthreadbackgroundtasks.thread;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
 import com.mzdhr.androidthreadbackgroundtasks.R;
+import com.mzdhr.androidthreadbackgroundtasks.task.MyJobDispatcher;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main8Activity extends AppCompatActivity {
+
+    private static boolean sIsJobDispatcherInitialized;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +32,52 @@ public class Main8Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Firebase Job Dispatcher
+
             }
         });
+    }
+
+
+    private void firebaseJobDispatcher(){
+        if (sIsJobDispatcherInitialized) {
+            return;
+        }
+
+        int INTERVAL_MINUTES = 1;
+        int INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(INTERVAL_MINUTES)) ;
+        int SYNC_FLEXTIME_SECONDS = INTERVAL_SECONDS;
+
+        GooglePlayDriver driver = new GooglePlayDriver(this);
+        FirebaseJobDispatcher firebaseJobDispatcher = new FirebaseJobDispatcher(driver);
+
+        Job ourJob = firebaseJobDispatcher.newJobBuilder()
+                .setService(MyJobDispatcher.class)
+                .setTag("MY_JOB_TAG")
+                .setConstraints(Constraint.DEVICE_CHARGING)
+                .setConstraints(Constraint.DEVICE_IDLE)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(
+                        Trigger.executionWindow(
+                                INTERVAL_SECONDS,
+                                INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS
+                        )
+                )
+                .setReplaceCurrent(true)
+                .build();
+
+        firebaseJobDispatcher.schedule(ourJob);
+
+        sIsJobDispatcherInitialized = true;
+    }
+
+    private void cancelFirebaseJobDispatcher(){
+        GooglePlayDriver driver = new GooglePlayDriver(this);
+        FirebaseJobDispatcher firebaseJobDispatcher = new FirebaseJobDispatcher(driver);
+
+        firebaseJobDispatcher.cancel("MY_JOB_TAG");
     }
 
 }
