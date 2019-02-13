6c83d097d45cfd6d15fe17f7c95ac2558839f37e
MohammadL <mmlaif@gmail.com>
2019-02-13 09:13:18 +0300

    feat: Add Activity for jobs

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/task/MyJobDispatcher.java
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/task/MyJobDispatcher.java
@@ -1,8 +1,30 @@
 package com.mzdhr.androidthreadbackgroundtasks.task;
 
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.mzdhr.androidthreadbackgroundtasks.pattern.AppExecutor;
+
 /**
  * Created by MohammadL on 13/2/2019
  * Contact me at mmlaif@gmail.com
  */
-public class MyJobDispatcher {
+public class MyJobDispatcher extends JobService {
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+        AppExecutor.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                // Our code goes here
+                // Our code goes here
+                // Our code goes here
+                jobFinished(job, false);
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        return false;
+    }
 }
