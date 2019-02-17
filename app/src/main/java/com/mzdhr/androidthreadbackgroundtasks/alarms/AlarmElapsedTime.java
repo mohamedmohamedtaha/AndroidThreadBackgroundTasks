557c4b9f06e03f6a91c5b94f705c04069fcbdce5
MohammadL <mmlaif@gmail.com>
2019-02-17 14:34:33 +0300

    feat: Add broadcat class for alarm

--- app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/alarms/AlarmElapsedTime.java
+++
@@ -1,8 +0,0 @@
-package com.mzdhr.androidthreadbackgroundtasks.alarms;
-
-/**
- * Created by MohammadL on 13/2/2019
- * Contact me at mmlaif@gmail.com
- */
-public class AlarmElapsedTime {
-}
---
+++ app/src/main/java/com/mzdhr/androidthreadbackgroundtasks/alarms/BroadcastForAlarm.java
@@ -0,0 +1,18 @@
+package com.mzdhr.androidthreadbackgroundtasks.alarms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by MohammadL on 13/2/2019
+ * Contact me at mmlaif@gmail.com
+ */
+public class BroadcastForAlarm extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
